e13a3f48f2a2b47fbb675fcb3b289e7c210a379b
jardmclan <mcleanj@hawaii.edu>
2024-03-07 09:50:50 -1000

    update dataDateMkr function

--- code/source/dataDateFunc.R
+++ code/source/dataDateFunc.R
@@ -2,19 +2,14 @@
 #date defining function
 dataDateMkr <- function(dateVar=NA){
   #try get date from source if exist 
-  globalDate<-commandArgs(trailingOnly=TRUE)[1] #pull from outside var when sourcing script
+  args<-commandArgs(trailingOnly=TRUE) #pull from outside var when sourcing script
   #make globalDate if 
-  globalDate<-if(is.na(globalDate) & !is.na(dateVar)) {
-    as.Date(dateVar) #if globalDate is NA & dateVar is not NA, set date in code with dateVar
-  } else {
-    if(exists("globalDate")) {
-      as.Date(globalDate)
-      } else { NA }
-  } 
-  dataDate<-if(!is.na(globalDate)){
-    globalDate-1 #make dataDate from globalDate
+  dataDate<-if(length(args) > 0) {
+    as.Date(args[0]) #if globalDate is NA & dateVar is not NA, set date in code with dateVar
+  } else if(!is.na(dateVar)) {
+    as.Date(dateVar)
   } else {
     Sys.Date()-1 #or sysDate -1
-  }  
+  }
   return(dataDate)
 }
